74dfab8089283bb3144db264ced5bb4d9663e343
BLON333 <jasonmblondeau@gmail.com>
2025-05-29 16:31:56 -0400

    Delete core directory

--- core/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-import os
-
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
-logging.basicConfig(level=LOG_LEVEL, format="%(message)s")
-
-def get_logger(name: str = __name__) -> logging.Logger:
-    return logging.getLogger(name)
